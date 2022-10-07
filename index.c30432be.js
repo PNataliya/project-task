b42a3c924fdce04be9dee3635aa4085eb486d1bc
PNataliya <pogrebnyak.nataliya@gmail.com>
2022-10-07 14:51:10 +0000

    Deploying to gh-pages from @ PNataliya/project-task@777562fca52d185988e41f121f656b6484af522b 🚀

--- index.c30432be.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll('input[type="range"]'),t=document.querySelector('input[type="number"]');function n(e){let t=e.target;"range"!==e.target.type&&(t=document.getElementById("range"));const n=t.min,r=t.max,u=t.value;t.style.backgroundSize=100*(u-n)/(r-n)+"% 100%"}e.forEach((e=>{e.addEventListener("input",n)})),t.addEventListener("input",n);
-//# sourceMappingURL=index.c30432be.js.map
